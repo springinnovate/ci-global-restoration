983e0d0294c0028e799540a6cdad8358470413a1
Rich Sharp <richpsharp@gmail.com>
2021-10-15 16:04:57 -0700

    track an overall script to run the pipeline

---
+++ run_pipeline.py
@@ -0,0 +1,36 @@
+"""Pipeline to manage all data and models."""
+import argparse
+import logging
+import os
+
+from osgeo import gdal
+from ecoshard import geoprocessing
+import numpy
+import taskgraph
+
+gdal.SetCacheMax(2**30)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'))
+logging.getLogger('taskgraph').setLevel(logging.WARN)
+LOGGER = logging.getLogger(__name__)
+
+WORKSPACE_DIR = 'ci_pipeline_workspace'
+
+
+def main():
+    """Entry point."""
+    parser = argparse.ArgumentParser(description='manage CI pipeline')
+    args = parser.parse_args()
+
+    os.makedirs(WORKSPACE_DIR, exist_ok=True)
+    task_graph = taskgraph.TaskGraph(WORKSPACE_DIR, -1)
+
+    task_graph.join()
+    task_graph.close()
+
+
+if __name__ == '__main__':
+    main()
