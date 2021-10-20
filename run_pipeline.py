c60ea7f88b0b63d9c56df72f4d5aa8f759c0bab5
Rich Sharp <richpsharp@gmail.com>
2021-10-20 13:27:53 -0700

    created functionality to break up by watershed group into pre-made watershed files

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,5 @@
 """Entry point to manage data and run pipeline."""
+import collections
 import glob
 import itertools
 import logging
@@ -7,8 +8,11 @@ import os
 import shutil
 import gzip
 
+from ecoshard import geoprocessing
 from ecoshard import taskgraph
 from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
 import ecoshard
 import requests
 
@@ -30,7 +34,8 @@ WORKSPACE_DIR = 'workspace'
 ECOSHARD_MAP = {
     'ESA_LULC': 'https://storage.googleapis.com/ecoshard-root/esa_lulc_smoothed/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1_md5_2ed6285e6f8ec1e7e0b75309cc6d6f9f.tif',
     'Scenario1_LULC': 'None',
-    'Biophysical table': 'None',
+    'sdr_biophysical_table': 'https://storage.googleapis.com/global-invest-sdr-data/Biophysical_table_ESA_ARIES_RS_md5_e16587ebe01db21034ef94171c76c463.csv',
+    'ndr_biophysical_table': 'https://storage.googleapis.com/nci-ecoshards/nci-NDR-biophysical_table_ESA_ARIES_RS3_md5_74d69f7e7dc829c52518f46a5a655fb8.csv',
     'DEM': 'https://storage.googleapis.com/global-invest-sdr-data/global_dem_3s_md5_22d0c3809af491fa09d03002bdf09748.zip',
     'Erosivity': 'https://storage.googleapis.com/global-invest-sdr-data/GlobalR_NoPol_compressed_md5_49734c4b1c9c94e49fffd0c39de9bf0c.tif',
     'Erodibility': 'https://storage.googleapis.com/ecoshard-root/pasquale/Kfac_SoilGrid1km_GloSEM_v1.1_md5_e1c74b67ad7fdaf6f69f1f722a5c7dfb.tif',
@@ -175,17 +180,239 @@ def fetch_and_unpack_data(task_graph):
         args=(file_map['DEM'], dem_dir, -9999, dem_vrt_path),
         target_path_list=[dem_vrt_path],
         task_name=f'unpack {file_map["DEM"]}')
+    file_map['DEM'] = dem_vrt_path
     for compressed_id in ['Waves', 'Watersheds']:
         _ = task_graph.add_task(
             func=_unpack_archive,
             args=(file_map[compressed_id], data_dir),
+            transient_run=True,
             task_name=f'decompress {file_map[compressed_id]}')
+        file_map[compressed_id] = data_dir
     LOGGER.debug('wait for unpack')
     task_graph.join()
     task_graph.close()
 
-def _run_sdr():
-    pass
+    # just need the base directory for watersheds
+    file_map['Watersheds'] = os.path.join(
+        file_map['Watersheds'], 'watersheds_globe_HydroSHEDS_15arcseconds')
+
+    # only need to lose the .gz on the waves
+    file_map['Waves'] = os.path.splitext(file_map['Waves'])
+
+    return file_map
+
+
+def _batch_into_watershed_subsets(
+        watershed_root_dir, degree_separation, watershed_subset=None):
+    """Construct geospatially adjacent subsets.
+
+    Breaks watersheds up into geospatially similar watersheds and limits
+    the upper size to no more than specified area. This allows for a
+    computationally efficient batch to run on a large contiguous area in
+    parallel while avoiding batching watersheds that are too small.
+
+    Args:
+        watershed_root_dir (str): path to watershed .shp files.
+        degree_separation (int): a blocksize number of degrees to coalasce
+            watershed subsets into.
+        watershed_subset (dict): if not None, keys are watershed basefile
+            names and values are FIDs to select. If present the simulation
+            only constructs batches from these watershed/fids, otherwise
+            all watersheds are run.
+
+    Returns:
+        list of (job_id, watershed.gpkg) tuples where the job_id is a
+        unique identifier for that subwatershed set and watershed.gpkg is
+        a subset of the original global watershed files.
+
+    """
+    # ensures we don't have more than 1000 watersheds per job
+    task_graph = taskgraph.TaskGraph(
+        watershed_root_dir, multiprocessing.cpu_count(), 10)
+    watershed_path_list = []
+    job_id_set = set()
+    for watershed_path in glob.glob(
+            os.path.join(watershed_root_dir, '*.shp')):
+        LOGGER.debug(f'scheduling {os.path.basename(watershed_path)}')
+        subbatch_job_index_map = collections.defaultdict(int)
+        # lambda describes the FIDs to process per job, the list of lat/lng
+        # bounding boxes for each FID, and the total degree area of the job
+        watershed_fid_index = collections.defaultdict(
+            lambda: [list(), list(), 0])
+        watershed_basename = os.path.splitext(
+            os.path.basename(watershed_path))[0]
+        watershed_ids = None
+        watershed_vector = gdal.OpenEx(watershed_path, gdal.OF_VECTOR)
+        watershed_layer = watershed_vector.GetLayer()
+
+        if watershed_subset:
+            if watershed_basename not in watershed_subset:
+                continue
+            else:
+                # just grab the subset
+                watershed_ids = watershed_subset[watershed_basename]
+                watershed_layer = [
+                    watershed_layer.GetFeature(fid) for fid in watershed_ids]
+
+        # watershed layer is either the layer or a list of features
+        for watershed_feature in watershed_layer:
+            fid = watershed_feature.GetFID()
+            watershed_geom = watershed_feature.GetGeometryRef()
+            watershed_centroid = watershed_geom.Centroid()
+            epsg = geoprocessing.get_utm_zone(
+                watershed_centroid.GetX(), watershed_centroid.GetY())
+            if watershed_geom.Area() > 1 or watershed_ids:
+                # one degree grids or immediates get special treatment
+                job_id = (f'{watershed_basename}_{fid}', epsg)
+                watershed_fid_index[job_id][0] = [fid]
+            else:
+                # clamp into degree_separation squares
+                x, y = [
+                    int(v//degree_separation)*degree_separation for v in (
+                        watershed_centroid.GetX(), watershed_centroid.GetY())]
+                base_job_id = f'{watershed_basename}_{x}_{y}'
+                # keep the epsg in the string because the centrid might lie
+                # on a different boundary
+                job_id = (f'''{base_job_id}_{
+                    subbatch_job_index_map[base_job_id]}_{epsg}''', epsg)
+                if len(watershed_fid_index[job_id][0]) > 1000:
+                    subbatch_job_index_map[base_job_id] += 1
+                    job_id = (f'''{base_job_id}_{
+                        subbatch_job_index_map[base_job_id]}_{epsg}''', epsg)
+                watershed_fid_index[job_id][0].append(fid)
+            watershed_envelope = watershed_geom.GetEnvelope()
+            watershed_bb = [watershed_envelope[i] for i in [0, 2, 1, 3]]
+            watershed_fid_index[job_id][1].append(watershed_bb)
+            watershed_fid_index[job_id][2] += watershed_geom.Area()
+
+        watershed_geom = None
+        watershed_feature = None
+
+        watershed_subset_dir = os.path.join(
+            watershed_root_dir, 'watershed_subsets')
+        os.makedirs(watershed_subset_dir, exist_ok=True)
+
+        for (job_id, epsg), (fid_list, watershed_envelope_list, area) in \
+                sorted(
+                    watershed_fid_index.items(), key=lambda x: x[1][-1],
+                    reverse=False):
+            if job_id in job_id_set:
+                raise ValueError(f'{job_id} already processed')
+            job_id_set.add(job_id)
+
+            watershed_subset_path = os.path.join(
+                watershed_subset_dir, f'{job_id}_a{area:.3f}.gpkg')
+            if not os.path.exists(watershed_subset_path):
+                task_graph.add_task(
+                    func=_create_fid_subset,
+                    args=(
+                        watershed_path, fid_list, epsg, watershed_subset_path),
+                    target_path_list=[watershed_subset_path],
+                    task_name=job_id)
+            watershed_path_list.append(watershed_path)
+
+        watershed_layer = None
+        watershed_vector = None
+
+    task_graph.join()
+    task_graph.close()
+    task_graph = None
+    return watershed_path_list
+
+
+def _create_fid_subset(
+        base_vector_path, fid_list, target_epsg, target_vector_path):
+    """Create subset of vector that matches fid list, projected into epsg."""
+    vector = gdal.OpenEx(base_vector_path, gdal.OF_VECTOR)
+    layer = vector.GetLayer()
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(target_epsg)
+    layer.SetAttributeFilter(
+        f'"FID" in ('
+        f'{", ".join([str(v) for v in fid_list])})')
+    gpkg_driver = ogr.GetDriverByName('gpkg')
+    subset_vector = gpkg_driver.CreateDataSource(target_vector_path)
+    subset_vector.CopyLayer(
+        layer, os.path.basename(os.path.splitext(target_vector_path)[0]))
+    subset_vector = None
+    layer = None
+    vector = None
+
+
+def _run_sdr(
+        workspace_dir,
+        watershed_subset,
+        dem_path,
+        erosivity_path,
+        erodibility_path,
+        lulc_path,
+        target_pixel_size,
+        biophysical_table_lucode_field,
+        threshold_flow_accumulation,
+        c_factor_path,
+        l_cap,
+        k_param,
+        sdr_max,
+        ic_0_param,
+        target_stitch_raster_map,
+        keep_intermediate_files=False,
+        ):
+    """Run SDR component of the pipeline.
+
+    This function will iterate through the watershed subset list, run the SDR
+    model on those subwatershed regions, and stitch those data back into a
+    global raster.
+
+    Args:
+        workspace_dir (str): path to directory to do all work
+        watershed_subset (list of list): lists of subwatersheds to process
+            in batch. Each element in the list is a
+            (shapefile_path, [fid1, fid2]) tuple where the fids refer to the
+            features in the given shapefile.
+        dem_path (str): path to global DEM raster
+        erosivity_path (str): path to global erosivity raster
+        erodibility_path (str): path to erodability raster
+        lulc_path (str): path to landcover raster
+        target_pixel_size (float): target projected pixel unit size
+        biophysical_table_lucode_field (str): name of the lucode field in
+            the biophysical table column
+        threshold_flow_accumulation (float): flow accumulation threshold
+            to use to calculate streams.
+        c_factor_path (float): path to c factor that's used for lucodes
+            that use the raster
+        l_cap (float): upper limit to the L factor
+        k_param (float): k parameter in SDR model
+        sdr_max (float): max SDR value
+        ic_0_param (float): IC0 constant in SDR model
+        target_stitch_raster_map (dict): maps the local path of an output
+            raster of this model to an existing global raster to stich into.
+        keep_intermediate_files (bool): if True, the intermediate watershed
+            workspace created underneath `workspace_dir` is deleted.
+
+    Returns:
+        None.
+    """
+
+    # Iterate through each watershed subset and run SDR
+    # stitch the results of whatever outputs to whatever global output raster.
+
+
+    args = {
+        'workspace_dir': '',
+        'results_suffix': '',
+        'dem_path': '',
+        'erosivity_path': '',
+        'erodibility_path': '',
+        'lulc_path': '',
+        'watersheds_path': '',
+        'biophysical_table_path': '',
+        'threshold_flow_accumulation': '',
+        'k_param': '',
+        'sdr_max': 0.8,
+        'ic_0_param': '',
+        'target_pixel_size': '',
+        'biophysical_table_lucode_field': '',
+    }
 
 
 def _run_ndr():
@@ -212,7 +439,19 @@ def main():
     """Entry point."""
     task_graph = taskgraph.TaskGraph(
         WORKSPACE_DIR, multiprocessing.cpu_count(), parallel_mode='thread')
-    fetch_and_unpack_data(task_graph)
+    data_map = fetch_and_unpack_data(task_graph)
+
+    watershed_subset = {
+        'af_bas_15s_beta': [19039, 23576, 18994],
+        'au_bas_15s_beta': [125804],
+        }
+    watershed_subset = None
+
+    watershed_batch_list = (
+        _batch_into_watershed_subsets(
+            data_map['Watersheds'], 10, watershed_subset))
+
+    LOGGER.debug(len(watershed_batch_list))
 
 
 if __name__ == '__main__':
