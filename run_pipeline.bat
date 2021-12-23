947f25714b189de8d10def62af67077263e57e0d
Rich Sharp <rich@therealspring.org>
2021-12-23 10:49:21 -0800

    try this with a thread to see what's going on

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -1,23 +1,23 @@
 @echo off
 :: SDR NDR in the current directory -- READY TO RUN
-python run_ndr_sdr_pipeline.py
-touch DONE_SDR_NDR
+rem python run_ndr_sdr_pipeline.py
+rem touch DONE_SDR_NDR
 
-:: Pollination -- READY TO RUN
-git clone https://github.com/therealspring/pollination_sufficiency
-cd pollination_sufficiency
-echo RUN POLLINATION
-wget -nc https://storage.googleapis.com/ecoshard-root/ci_global_restoration/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46.tif
-wget -nc https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
-wget -nc https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
-python make_poll_suff.py ./ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46.tif
-wget -nc https://storage.googleapis.com/ecoshard-root/ci_global_restoration/Sc2_Griscom_CookPatton_smithpnv_md5_1536327d82e292529e7872dc6ecc2871.tif
-python make_poll_suff.py ./Sc2_Griscom_CookPatton_smithpnv_md5_1536327d82e292529e7872dc6ecc2871.tif
-python calc_people_fed.py
-python realized_pollination.py pollination_ppl_fed_on_ag_10s_esa2020mVCF.tif workspace_poll_suff\churn\hab_mask\ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46_hab_mask.tif
-python realized_pollination.py pollination_ppl_fed_on_ag_10s_Sc2_Griscom_CookPatton.tif workspace_poll_suff\churn\hab_mask\Sc2_Griscom_CookPatton_smithpnv_md5_1536327d82e292529e7872dc6ecc2871_hab_mask.tif
-cd ..
-touch DONE_POLLINATION
+rem :: Pollination -- READY TO RUN
+rem git clone https://github.com/therealspring/pollination_sufficiency
+rem cd pollination_sufficiency
+rem echo RUN POLLINATION
+rem wget -nc https://storage.googleapis.com/ecoshard-root/ci_global_restoration/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46.tif
+rem wget -nc https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
+rem wget -nc https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
+rem python make_poll_suff.py ./ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46.tif
+rem wget -nc https://storage.googleapis.com/ecoshard-root/ci_global_restoration/Sc2_Griscom_CookPatton_smithpnv_md5_1536327d82e292529e7872dc6ecc2871.tif
+rem python make_poll_suff.py ./Sc2_Griscom_CookPatton_smithpnv_md5_1536327d82e292529e7872dc6ecc2871.tif
+rem python calc_people_fed.py
+rem python realized_pollination.py pollination_ppl_fed_on_ag_10s_esa2020mVCF.tif workspace_poll_suff\churn\hab_mask\ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46_hab_mask.tif
+rem python realized_pollination.py pollination_ppl_fed_on_ag_10s_Sc2_Griscom_CookPatton.tif workspace_poll_suff\churn\hab_mask\Sc2_Griscom_CookPatton_smithpnv_md5_1536327d82e292529e7872dc6ecc2871_hab_mask.tif
+rem cd ..
+rem touch DONE_POLLINATION
 
 :: CV -- READY TO RUN
 git clone https://github.com/therealspring/cnc_global_cv
@@ -27,7 +27,7 @@ popd
 touch DONE_CV
 
 :: Coastal beneficiares
-git clone https://github.com/therealspring/people_protected_by_coastal_habitat
+rem git clone https://github.com/therealspring/people_protected_by_coastal_habitat
 
 :: need to copy the following rasters over from global_cv_analysis.py called habitat_value_raster_path on line 2163
 rem mkdir people_protected_by_coastal_habitat\value_rasters
