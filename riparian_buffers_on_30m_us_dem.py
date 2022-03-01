431842bed273071c5556b65b2f138609b4a0f6a1
Rich Sharp <rich@therealspring.org>
2022-03-01 13:29:54 -0800

    adding the rest of the pipeline

--- riparian_buffers_on_30m_us_dem.py
+++ riparian_buffers_on_30m_us_dem.py
@@ -11,7 +11,7 @@ gdal.SetCacheMax(2**26)
 _LARGEST_BLOCK = 2**26
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format=(
         '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
         ' [%(pathname)s.%(funcName)s:%(lineno)d] %(message)s'))
@@ -30,6 +30,7 @@ def main():
     filled_dem_raster_path = os.path.join(
         WORKSPACE_DIR, f'filled_{os.path.basename(dem_raster_path)}.tif')
 
+    LOGGER.info('fill pits')
     fill_task = task_graph.add_task(
         func=routing.fill_pits,
         args=((dem_raster_path, 1), filled_dem_raster_path),
@@ -40,33 +41,62 @@ def main():
         target_path_list=[filled_dem_raster_path],
         task_name=f'fill dem at {dem_raster_path}')
 
+    LOGGER.info('flow dir')
     flow_dir_raster_path = os.path.join(
         WORKSPACE_DIR, f'd8_{os.path.basename(dem_raster_path)}.tif')
-    route_task = task_graph.add_task(
+    flow_dir_task = task_graph.add_task(
         func=routing.flow_dir_d8,
         args=((filled_dem_raster_path, 1), flow_dir_raster_path),
         target_path_list=[flow_dir_raster_path],
         dependent_task_list=[fill_task],
         task_name=f'route {dem_raster_path}')
 
+    flow_accum_raster_path = os.path.join(
+        WORKSPACE_DIR, f'flow_accum_{os.path.basename(dem_raster_path)}')
+    flow_accum_task = task_graph.add_task(
+        func=routing.flow_accumulation_d8,
+        args=(
+            (flow_dir_raster_path, 1), flow_accum_raster_path),
+        target_path_list=[flow_accum_raster_path],
+        dependent_task_list=[flow_dir_task],
+        task_name=f'flow accum for {flow_dir_raster_path}')
+
+    stream_vector_path = os.path.join(WORKSPACE_DIR, f'''streams_{
+        os.path.basename(os.path.splitext(dem_raster_path)[0])}.gpkg''')
+    extract_streams_task = task_graph.add_task(
+        func=routing.extract_strahler_streams_d8,
+        args=(
+            (flow_dir_raster_path, 1), (flow_accum_raster_path, 1),
+            (filled_dem_raster_path, 1),
+            stream_vector_path),
+        target_path_list=[stream_vector_path],
+        dependent_task_list=[flow_dir_task, flow_accum_task],
+        task_name=f'extract streams {flow_dir_raster_path}')
+
+    watershed_boundary_vector_path = os.path.join(
+        WORKSPACE_DIR, f'''subwatersheds_{
+            os.path.basename(os.path.splitext(dem_raster_path)[0])}.gpkg''')
+    calc_watershed_task = task_graph.add_task(
+        func=routing.calculate_subwatershed_boundary,
+        args=(
+            (flow_dir_raster_path, 1), stream_vector_path,
+            watershed_boundary_vector_path),
+        target_path_list=[watershed_boundary_vector_path],
+        dependent_task_list=[flow_dir_task, extract_streams_task],
+        task_name=f'calc watershed for {stream_vector_path}')
+
+    LOGGER.info('detect outlets')
     outlet_vector_path = os.path.join(WORKSPACE_DIR, f'''outlets_{
         os.path.basename(os.path.splitext(dem_raster_path)[0])}.gpkg''')
     detect_outlets_task = task_graph.add_task(
         func=routing.detect_outlets,
         args=((flow_dir_raster_path, 1), 'd8', outlet_vector_path),
         target_path_list=[outlet_vector_path],
-        dependent_task_list=[route_task],
+        dependent_task_list=[flow_dir_task],
         task_name=f'detect outlets on {flow_dir_raster_path}')
 
-    # task_graph.add_task(
-    #     func=extract_strahler_streams_d8,
-    #     args=(
-    #         (flow_dir_raster_path, 1), (flow_accum_path, 1),
-    #         (filled_dem_raster_path, 1),
-    #         stream_vector_path),
-    #     target_path_list=[stream_vector_path],
-    #     dependent_task_list=[route_task, flow_accum_task],
-    #     task_name=f'extract streams {flow_dir_raster_path}')
+    task_graph.close()
+    task_graph.join()
 
 
 if __name__ == '__main__':
