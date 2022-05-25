3e857c03522cd24527b3c9d81fb7b6386d4d3891
Rich Sharp <richpsharp@gmail.com>
2022-05-25 09:24:22 -0700

    default to the first projection so we don't get a case where there's rasters with different projections

--- create_scenario.py
+++ create_scenario.py
@@ -111,6 +111,8 @@ def main():
             aligned_raster_path_list, align_mode_list,
             lulc_info['pixel_size'], 'union'),
         target_path_list=aligned_raster_path_list,
+        target_projection_wkt=geoprocessing.get_raster_info(
+            base_raster_path_list[0])['projection_wkt'],
         task_name='align raster stack')
 
     if args.flip_target_path:
